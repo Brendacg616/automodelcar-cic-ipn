de1c5f90235d1d1e115d82fc866505f4da13fc9d
Cesar Bravo <conilo@gmail.com>
2017-12-01 15:55:51 -0600

    Igore build/ and devel/ folders.

--- build/cic/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/conilo/Workspace/automodelcar-cic-ipn/src/cic
-# Build directory: /home/conilo/Workspace/automodelcar-cic-ipn/build/cic
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
